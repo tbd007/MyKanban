f9a359178b618199023defd84e7614198ac83718
Tanja Babic-Debrosse <babicdebrosse@outlook.de>
2020-06-24 11:17:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MyApp from './components/App';
-import MyInfo from './components/MyInfo';
+
 import * as serviceWorker from './serviceWorker';
 
 
